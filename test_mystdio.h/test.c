9ee349b38b27eb56889d1b542bb99dfe3ae3d6e4
zincsweet <1099214139@qq.com>
2026-05-23 15:39:33 +0800

    增加了mystdio.h的demo

---
+++ test_mystdio.h/test.c
@@ -0,0 +1,23 @@
+#include "mystdio.h" 
+#include <unistd.h>
+
+
+int main() {
+	myFILE* fp = myfopen("log.txt", "w");
+	if (fp == NULL) {
+		printf("myfopen error!\n");
+		return 0;
+	}
+
+	int cnt = 10;
+	const char* str = "hello myio";
+	while (cnt--) {
+		myfputs(str, fp);
+		sleep(1);
+		printf("debug: outbuffer: %s, pos: %d\n", fp->outbuffer, fp->pos);
+	}
+
+	myfclose(fp);
+	printf("wtite file success!\n");
+	return 0;
+}
